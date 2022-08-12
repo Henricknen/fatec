3a780f6a19cdeb17ed0289d22f29ef2ce5376673
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-08-12 12:34:47 -0300

    alteracao

--- Program.cs
+++ Program.cs
@@ -2,9 +2,11 @@
 
 while (op != "3")
 {
+    Console.Clear();
     Console.WriteLine("menu principal");
     Console.WriteLine("1 - Funções sem Vetor");
     Console.WriteLine("2 - Funções com Vetor");
     Console.WriteLine("3 - Sair");
+    Console.Write("Digite uma opção: ");
     op = Console.ReadLine();
 }
