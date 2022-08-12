d93928761b867c0fd0886532af768958933dbb81
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-08-12 12:29:57 -0300

    primeira aula

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿string op = "0";
+
+while (op != "3")
+{
+    Console.WriteLine("menu principal");
+    Console.WriteLine("1 - Funções sem Vetor");
+    Console.WriteLine("2 - Funções com Vetor");
+    Console.WriteLine("3 - Sair");
+    op = Console.ReadLine();
+}
