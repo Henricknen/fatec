7146f732b3a50f91736755ecb89634fe2a8eb87c
Luis Henrique Silva Ferreira <56833636+Henricknen@users.noreply.github.com>
2022-08-19 12:11:39 -0300

    estrutura de dados

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿string op = "0";
+﻿string op = "0", op2;
 
 while (op != "3")
 {
@@ -9,4 +9,24 @@ while (op != "3")
     Console.WriteLine("3 - Sair");
     Console.Write("Digite uma opção: ");
     op = Console.ReadLine();
+
+    if (op == "1") 
+    {
+            Console.Write("Numero inicia: l");
+            int n1;
+            n1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Numero final: l");
+            int n2;
+            n2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("menu 2");
+            Console.WriteLine("1 - Crescente");
+            Console.WriteLine("2 - Decrescente: ");
+            Console.WriteLine("3 - Imparares: ");
+            Console.Write("4 - Somatorio: ");
+            Console.Write("Opção desejada: : ");
+            op2 = Console.ReadLine();
+    }
+    Console.ReadKey
 }
