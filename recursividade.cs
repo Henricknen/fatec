ccc9b04645c72d0f07384ae69193dcd176f53cb8
Luis Henrique Silva Ferreira <56833636+Henricknen@users.noreply.github.com>
2022-08-31 19:53:54 -0300

    estrutura de dados -recursividade

---
+++ recursividade.cs
@@ -0,0 +1,25 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# Visual Studio 15
+VisualStudioVersion = 15.0.28307.136
+MinimumVisualStudioVersion = 10.0.40219.1
+Project("{9A19103F-16F7-4668-BE54-9A1E7A4F7556}") = "Exerc01", "..\Exerc01\Exerc01.csproj", "{C3BA5C4C-1D49-4AA9-8009-61BAA0261493}"
+EndProject
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Debug|Any CPU = Debug|Any CPU
+		Release|Any CPU = Release|Any CPU
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{C3BA5C4C-1D49-4AA9-8009-61BAA0261493}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
+		{C3BA5C4C-1D49-4AA9-8009-61BAA0261493}.Debug|Any CPU.Build.0 = Debug|Any CPU
+		{C3BA5C4C-1D49-4AA9-8009-61BAA0261493}.Release|Any CPU.ActiveCfg = Release|Any CPU
+		{C3BA5C4C-1D49-4AA9-8009-61BAA0261493}.Release|Any CPU.Build.0 = Release|Any CPU
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {3396BFBB-846B-4CF2-BA4C-194B2EE25E65}
+	EndGlobalSection
+EndGlobal
