9c9d500308567528adde79e6e1b9ecf254c4675e
Luis Henrique Silva Ferreira <l.henrick@live.com>
2022-09-23 20:11:44 -0300

    recursividade

--- recursividade.cs
+++ recursividade.cs
@@ -1,25 +1,38 @@
-﻿
-Microsoft Visual Studio Solution File, Format Version 12.00
-# Visual Studio 15
-VisualStudioVersion = 15.0.28307.136
-MinimumVisualStudioVersion = 10.0.40219.1
-Project("{9A19103F-16F7-4668-BE54-9A1E7A4F7556}") = "Exerc01", "..\Exerc01\Exerc01.csproj", "{C3BA5C4C-1D49-4AA9-8009-61BAA0261493}"
-EndProject
-Global
-	GlobalSection(SolutionConfigurationPlatforms) = preSolution
-		Debug|Any CPU = Debug|Any CPU
-		Release|Any CPU = Release|Any CPU
-	EndGlobalSection
-	GlobalSection(ProjectConfigurationPlatforms) = postSolution
-		{C3BA5C4C-1D49-4AA9-8009-61BAA0261493}.Debug|Any CPU.ActiveCfg = Debug|Any CPU
-		{C3BA5C4C-1D49-4AA9-8009-61BAA0261493}.Debug|Any CPU.Build.0 = Debug|Any CPU
-		{C3BA5C4C-1D49-4AA9-8009-61BAA0261493}.Release|Any CPU.ActiveCfg = Release|Any CPU
-		{C3BA5C4C-1D49-4AA9-8009-61BAA0261493}.Release|Any CPU.Build.0 = Release|Any CPU
-	EndGlobalSection
-	GlobalSection(SolutionProperties) = preSolution
-		HideSolutionNode = FALSE
-	EndGlobalSection
-	GlobalSection(ExtensibilityGlobals) = postSolution
-		SolutionGuid = {3396BFBB-846B-4CF2-BA4C-194B2EE25E65}
-	EndGlobalSection
-EndGlobal
+using Sytem;
+
+class Calc {
+    public int soma(params int[]n) {
+        int s = 0;
+        for(int i = 0; i < n,length; i++) {
+            s += n[i];
+        }
+        return s;
+    }
+
+    public double soma(params double[n]) {
+        double s = 0;
+        for(int i = 0; i <n.length; i++) {
+            s += n[i];
+        }
+        return s;
+    }
+
+    public fat(int n) {
+        int res;
+        if(n <= 1) {
+            res = 1;
+        } else {
+            res = n * fat(n - 1);       /* operação de fatorial */
+        }
+        return res;
+    }
+
+}
+
+class Recursividade Main() {
+    Calc calc = new Calc();
+
+    var res = calc.fat(5);
+
+    Console.WriteLine(res);
+}
