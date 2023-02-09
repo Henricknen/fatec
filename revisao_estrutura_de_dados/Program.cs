8ce6366d3307260e87b82a11d99e720f6b4e4207
Luis Henrique Silva Ferreira <l.henrick@live.com>
2023-02-09 20:41:39 -0300

    fazendo revisao de estrutura de dado com c#

---
+++ revisao_estrutura_de_dados/Program.cs
@@ -0,0 +1,35 @@
+﻿int x;      // declaração de 'inteiro'
+int y;
+
+string nome;        // declarção de 'string'
+
+const int yy = 10;      // constante 'não pode' ser alterada
+
+x = 10 + 5;
+bool a = x > 10;
+Console.WriteLine(x);
+Console.WriteLine(a);
+
+Console.WriteLine("Digite um numero: ");
+x = Convert.ToInt32(Console.ReadLine());      // numero digitado será tribuido a variável 'x' e 'Convert.ToInt32' converte 'string' em 'int'
+
+Console.WriteLine("Digite um numero: ");
+y = Convert.ToInt32(Console.ReadLine()); 
+
+if(x > y) {
+    Console.WriteLine("V é maior que y");
+} else if(x == y) { 
+    Console.WriteLine("x é igual a y");
+} else {
+    Console.WriteLine("Y é igual a x");
+}
+
+
+
+// using System;
+
+// class Revisao_C# {
+//     static void main() {
+//         Console.WriteLine("Ola mundo");
+//     }
+// }
