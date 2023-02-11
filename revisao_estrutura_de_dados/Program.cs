e24e4f408a405704e13d66654200583344370e8f
Luis Henrique Silva Ferreira <l.henrick@live.com>
2023-02-10 22:12:28 -0300

    revisão

--- revisao_estrutura_de_dados/Program.cs
+++ revisao_estrutura_de_dados/Program.cs
@@ -5,24 +5,52 @@ string nome;        // declarção de 'string'
 
 const int yy = 10;      // constante 'não pode' ser alterada
 
+Console.WriteLine("-------bool----------------");
 x = 10 + 5;
 bool a = x > 10;
 Console.WriteLine(x);
 Console.WriteLine(a);
 
-Console.WriteLine("Digite um numero: ");
+Console.WriteLine("-------maior ou menor-----------------");
+Console.WriteLine("Digite um numero X: ");
+
 x = Convert.ToInt32(Console.ReadLine());      // numero digitado será tribuido a variável 'x' e 'Convert.ToInt32' converte 'string' em 'int'
 
-Console.WriteLine("Digite um numero: ");
+Console.WriteLine("Digite um numero Y: ");
 y = Convert.ToInt32(Console.ReadLine()); 
 
 if(x > y) {
-    Console.WriteLine("V é maior que y");
+    Console.WriteLine("x é maior que y");
 } else if(x == y) { 
     Console.WriteLine("x é igual a y");
 } else {
-    Console.WriteLine("Y é igual a x");
+    Console.WriteLine("y é maior a x");
 }
+
+Console.WriteLine("-------while-----------------");
+int i = 1;      //declarção
+while(i <= 5) {     // comando de repetição 'while'
+    Console.WriteLine(i);
+    i = i + 1;
+}
+
+Console.WriteLine("-------for-----------------");
+
+for (i = 1; i <= 5; i = i + 1) {
+    Console.WriteLine(i);
+}
+
+Console.WriteLine("-------vetor-----------------");
+
+int[] b = new int[5];
+
+b[0] = 10;
+b[1] = 20;
+b[2] = 30;
+
+for(i = 0; i <= 4; i++) 
+    Console.WriteLine(b[i]);
+
 
 
 
