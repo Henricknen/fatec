6962f2ddbb912285e5d0a4f3523200941e8982c0
Luis Henrique Silva Ferreira <l.henrick@live.com>
2023-02-23 20:29:24 -0300

    encerramentoda revisao de c #

--- Program.cs
+++ Program.cs
@@ -1,107 +1,63 @@
-﻿using System;
+﻿int x;
+string nome;
 
-namespace palavras_ao_contrario
+const int YY = 10;
+
+x = 10 + 5;
+bool a = x > 10;
+Console.WriteLine(x);
+Console.WriteLine(a);
+int y;
+Console.Write("Digite um numero: ");
+x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Digite um numero: ");
+y = Convert.ToInt32(Console.ReadLine());
+if (x>y)
+    Console.WriteLine("x maior que y");
+else if (x == y)
+    Console.WriteLine("x é igual a y");
+else
 {
-    class Program
-    {
+    Console.WriteLine("x menor q y");
+    Console.WriteLine("uso das chaves");
+}
+
+int i=1;
+while(i<=5)
+{
+    Console.WriteLine(i);
+    i = i + 1;
+}
+
+for (i=1; i<=5; i=i+1)
+    Console.WriteLine(i);
+
+int[] b = new int[5];
+b[0]=10;
+b[1]=20;
+b[2]=30;
+for(i=0;i<=4;i++)
+    Console.WriteLine(b[i]);
+
+int soma(int x, ref int y) {
+    x = 30;
+    y = 40;
+    return x + y;
+}
+
+int a2 = 10, b2 = 20, z;
+z = soma(a2, ref b2);
+Console.WriteLine("a2: " + a2 + " - b2: " + b2 + " - soma: " + z);
 
 
-        //const int MAX = int.MaxValue;
-        const int MAX = 20;
+Tipo c;     // declaração
+c = new Tipo();     // iniçialização
+c.codigo = 1;
+c.nome = "Luis henrique";
+Console.WriteLine(c.codigo + " " + c.nome);
 
-        // INICIO FUNÇÕES AUXILIARES DE PILHA
+class Tipo {
+    public int codigo;
+    public string nome;
 
-        static void Insere(char[] p, ref int t, char v)
-        {
-            p[t] = v;
-            t = t + 1;
-        }
-
-        static char Remove(char[] p, ref int t)
-        {
-            t = t - 1;
-            return (p[t]);
-        }
-
-
-
-        static bool EstaVazia(int t)
-        {
-            if (t == 0)
-                return true;
-            else
-                return false;
-        }
-
-      
-
-
-        static void Main(string[] args)
-        {
-            char[] pilha = new char[MAX];
-            int topo = 0;
-   
-            int i = 0;
-            string frase;
-            string nova_frase = "";
-
-            Console.WriteLine("Digite a frase: ");
-            frase = Console.ReadLine();
-
-            int tam = frase.Length;
-            char caractere;
-            bool ultimoChar = false;
-
-
-            while (i < tam)
-            {
-
-
-                if (i == tam - 1)
-                {
-
-                    ultimoChar = true;
-                }
-
-
-
-                caractere = frase[i];
-
-                if (caractere != ' ')
-                {
-
-                    Insere(pilha, ref topo, frase[i]);
-
-                    i++;
-
-                }
-
-                if (ultimoChar || caractere == ' ')
-                {
-
-                    while (!EstaVazia(topo))
-                    {
-
-                        char c = Remove(pilha, ref topo);
-                        nova_frase += c;
-                    }
-                    nova_frase += " ";
-
-                    i++;
-
-                }
-
-
-
-            }
-
-            Console.WriteLine(nova_frase);
-            Console.ReadKey();
-
-
-
-
-        }
-
-    }
 }
